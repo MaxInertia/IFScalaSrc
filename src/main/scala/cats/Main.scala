727df4bd6c78a8c32e7f36712feccea387add991
Dorian Thiessen (dbt908) <dorian.thiessen@usask.ca>
2018-04-13 02:00:38 -0500

    Add ghpages-sbt

---
+++ src/main/scala/cats/Main.scala
@@ -0,0 +1,8 @@
+package cats
+
+/**
+  * Created by Dorian Thiessen on 2018-04-12.
+  */
+class Main {
+
+}
---
+++ src/main/scala/languages/turing/Main.scala
@@ -0,0 +1,8 @@
+package languages.turing
+
+/**
+  * Created by Dorian Thiessen on 2018-03-31.
+  */
+class Main {
+
+}
